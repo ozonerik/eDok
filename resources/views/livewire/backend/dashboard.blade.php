c1cdf22ec51084a930882f07224d0de79704117f
ozonerik <ozonerik@gmail.com>
2022-02-11 11:35:54 +0700

    perbaikan layout

--- resources/views/livewire/backend/dashboard.blade.php
+++ resources/views/livewire/backend/dashboard.blade.php
@@ -4,10 +4,10 @@
     </h2>
 </x-slot>
 
-<div class="py-12">
+<div class="py-8">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="p-6 sm:px-16 bg-white border-b border-gray-200">
+            <div class="p-6 sm:px-6 bg-white border-b border-gray-200">
                 <div class="text-2xl">
                     Welcome to your Jetstream application!
                 </div>
