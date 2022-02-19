1d42ac795ec9e6b7c26c90a6ce3446403c8c5397
ozonerik <ozonerik@gmail.com>
2022-02-20 00:12:12 +0700

    fixed form & table menu component

--- resources/views/livewire/back/form/formotherfile-modal.blade.php
+++ resources/views/livewire/back/form/formotherfile-modal.blade.php
@@ -1,28 +1,17 @@
-<!-- Delete Otherfile Modal -->
-<div class="modal fade" wire:ignore.self id="form-del" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" >
-  <div class="modal-dialog modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Delete File Confirmation</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <form autocomplete="off" wire:submit.prevent="delete">
-      <div class="modal-body">
-        <div class="text-center mb-1">Do you want delete this @if($myfile_id) {{ count($myfile_id) }} @endif items ?</div>
-        <ol class="list-group list-group-numbered">
-        @foreach($delsel as $row)
-          <li class="list-group-item list-group-item-action">{{$row->name}} by {{$row->user->name}}</li>
-        @endforeach
-        </ol>
-        <div class="text-center mt-1">
-          <small class="text-danger">*All selected files will be permanently deleted</small>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary btn-sm text-light" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-danger btn-sm text-light">Delete</button>
-      </div>
-      </form>
+<!-- Delete Other file Modal -->
+<x-ModalForm modalname="form-del" linksubmit="delete" btntype="danger" btnlabel="Delete">
+  <x-slot:modaltitle>
+    Delete File Confirmation
+  </x-slot>
+  <x-slot:modalbody>
+    <div class="text-center mb-1">Do you want delete this @if($myfile_id) {{ count($myfile_id) }} @endif items ?</div>
+    <ol class="list-group list-group-numbered">
+    @foreach($delsel as $row)
+      <li class="list-group-item list-group-item-action">{{$row->name}} by {{$row->user->name}}</li>
+    @endforeach
+    </ol>
+    <div class="text-center mt-1">
+      <small class="text-danger">*All selected files will be permanently deleted</small>
     </div>
-  </div>
-</div>
+  </x-slot>
+</x-ModalForm>
--- resources/views/livewire/back/publicfile.blade.php
+++ resources/views/livewire/back/publicfile.blade.php
@@ -17,69 +17,48 @@
 @endpush
 <div>
     <x-LoadingState />
-    <div class="row justify-content-center my-5">
-        <div class="col-md-12">
-            <div class="card shadow bg-light">
-                <div class="card-body bg-white px-5 py-3 border-bottom rounded-top">
-                    <div class="mx-3 my-3">
-                        <!-- table menu -->
-                        <div class="d-flex flex-column flex-md-row mb-3">
-                            <div class="mb-2 mb-md-0 me-md-2">
-                                <div class="input-group">
-                                    <span class="input-group-text">Per Page :</span>
-                                    <select wire:model="perhal" class="form-select">
-                                    <option value="2" selected>2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="{{$myfile->total()}}">All</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="flex-fill">
-                                <input type="text" wire:model.debounce.500ms="inpsearch" class="form-control" placeholder="Search...">
-                            </div>
-                        </div>
-                        <!-- .table menu -->
-                        <!-- table -->
-                        <x-TableSlot :table="$myfile" ncol="7">
-                            <x-slot:thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th style="cursor:pointer;" wire:click="sortBy('category_name')"><x-SortState colName="category_name"  :sortBy="$sortBy" :sortDir="$sortDirection">Category</x-SortState></th>
-                                    <th style="cursor:pointer;" wire:click="sortBy('name')"><x-SortState colName="name"  :sortBy="$sortBy" :sortDir="$sortDirection">File Name</x-SortState></th>
-                                    <th style="cursor:pointer;" wire:click="sortBy('user_name')"><x-SortState colName="user_name"  :sortBy="$sortBy" :sortDir="$sortDirection">Owner</x-SortState></th>
-                                    <th style="cursor:pointer;" wire:click="sortBy('file_size')"><x-SortState colName="file_size"  :sortBy="$sortBy" :sortDir="$sortDirection">File Size</x-SortState></th>
-                                    <th style="cursor:pointer;" wire:click="sortBy('updated_at')"><x-SortState colName="updated_at"  :sortBy="$sortBy" :sortDir="$sortDirection">Updated At</x-SortState></th>
-                                    <th>Action</th>
-                                </tr>
-                            </x-slot>
-                            <x-slot:tbody>
-                                @foreach($myfile as $key => $row)
-                                <tr>
-                                    <td>{{ $myfile->firstItem() + $key}}</td>
-                                    <td>{{ $row->category_name }}</td>
-                                    <td>{{ $row->name }}</td>
-                                    <td>{{ $row->user_name }}</td>
-                                    <td>{{ convert_bytes($row->file_size) }}</td>
-                                    <td>{{ $row->updated_at }}</td>
-                                    <td>
-                                        @if(Storage::disk('public')->exists($row->path))
-                                        <button wire:click.prevent="export({{$row->id}})" class="btn btn-success btn-sm text-light me-1 mb-2 mb-md-0" data-bs-toggle="tooltip" data-bs-placement="top" title="Download"><i class="bi bi-cloud-arrow-down-fill"></i></button>
-                                        @else
-                                        <button class="btn btn-secondary btn-sm text-light me-1 mb-2 mb-md-0" data-bs-toggle="tooltip" data-bs-placement="top" title="Download" disabled><i class="bi bi-cloud-arrow-down-fill"></i></button>
-                                        @endif
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </x-slot>
-                        </x-TableSlot>
-                        <!-- .table -->
-                        <!-- pagination -->
-                        <x-paginating :table="$myfile" />
-                        <!-- .pagination -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-CardLayout>
+        <!-- table menu -->
+        <x-TableMenu mdperhal="perhal" :table="$myfile" :checked="[]" mdsearch="inpsearch">
+            <x-slot:dropdownmenu></x-slot>
+        </x-TableMenu>
+        <!-- .table menu -->
+        <!-- table -->
+        <x-TableSlot :table="$myfile" ncol="7">
+            <x-slot:thead>
+                <tr>
+                    <th>No</th>
+                    <th style="cursor:pointer;" wire:click="sortBy('category_name')"><x-SortState colName="category_name"  :sortBy="$sortBy" :sortDir="$sortDirection">Category</x-SortState></th>
+                    <th style="cursor:pointer;" wire:click="sortBy('name')"><x-SortState colName="name"  :sortBy="$sortBy" :sortDir="$sortDirection">File Name</x-SortState></th>
+                    <th style="cursor:pointer;" wire:click="sortBy('user_name')"><x-SortState colName="user_name"  :sortBy="$sortBy" :sortDir="$sortDirection">Owner</x-SortState></th>
+                    <th style="cursor:pointer;" wire:click="sortBy('file_size')"><x-SortState colName="file_size"  :sortBy="$sortBy" :sortDir="$sortDirection">File Size</x-SortState></th>
+                    <th style="cursor:pointer;" wire:click="sortBy('updated_at')"><x-SortState colName="updated_at"  :sortBy="$sortBy" :sortDir="$sortDirection">Updated At</x-SortState></th>
+                    <th>Action</th>
+                </tr>
+            </x-slot>
+            <x-slot:tbody>
+                @foreach($myfile as $key => $row)
+                <tr>
+                    <td>{{ $myfile->firstItem() + $key}}</td>
+                    <td>{{ $row->category_name }}</td>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->user_name }}</td>
+                    <td>{{ convert_bytes($row->file_size) }}</td>
+                    <td>{{ $row->updated_at }}</td>
+                    <td>
+                        @if(Storage::disk('public')->exists($row->path))
+                        <button wire:click.prevent="export({{$row->id}})" class="btn btn-success btn-sm text-light me-1 mb-2 mb-md-0" data-bs-toggle="tooltip" data-bs-placement="top" title="Download"><i class="bi bi-cloud-arrow-down-fill"></i></button>
+                        @else
+                        <button class="btn btn-secondary btn-sm text-light me-1 mb-2 mb-md-0" data-bs-toggle="tooltip" data-bs-placement="top" title="Download" disabled><i class="bi bi-cloud-arrow-down-fill"></i></button>
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+            </x-slot>
+        </x-TableSlot>
+        <!-- .table -->
+        <!-- pagination -->
+        <x-paginating :table="$myfile" />
+        <!-- .pagination -->
+    </x-CardLayout>
 </div>
