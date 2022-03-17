a2a16b8c2b3cad8a3eedbf67628c9cb460f196e6
ozonerik <ozonerik@gmail.com>
2022-03-17 10:49:37 +0700

    update home

--- resources/views/livewire/front/sections/section1.blade.php
+++ resources/views/livewire/front/sections/section1.blade.php
@@ -1,4 +1,4 @@
-<section class="clean-block clean-hero" style="background-image:url('{{asset('img/flat-lay-paper-mockup-laptop.jpg')}}');color:rgba(9, 162, 255, 0.85);">
+<section class="clean-block clean-hero" style="background-image:url('{{asset('img/tech/image2.jpg')}}');color:rgba(9, 162, 255, 0.85);">
     <div class="text row">
         <div class="col-12 h2 text-light">Jumlah File Terupload Tahun    {{ \Carbon\Carbon::now()->format('Y')}}</div>
         <div class="col-12 mb-4" wire:ignore>
